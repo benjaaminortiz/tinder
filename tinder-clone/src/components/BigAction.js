35be6ecd2a278eacc74078f5b8f1b41c193909e3
benjaaminortiz <Benjask89@icloud.com>
2022-07-27 11:19:21 -0300

    files

---
+++ tinder-clone/src/components/BigAction.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const BigAction = (icon) => {
+  return (
+    <div>
+        <button onClick={() => console.log('Pressed')}>Hello World</button>
+    </div>
+  )
+}
+
+export default BigAction
